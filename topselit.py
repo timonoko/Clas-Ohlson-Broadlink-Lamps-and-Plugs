e2bee9f5cae27ef2bb8ab34cd086182725755d14
Timo Noko <timonoko@users.noreply.github.com>
2021-08-12 07:17:16 +0300

    --

--- topselit.py
+++ topselit.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 
-# "Jaahdytin|7"-named plug ON/OFF webpy-server at localhost:8083
 
 import os
 import math
@@ -9,6 +8,8 @@ import urllib
 import web
 import broadlink
 
+os.chdir('/home/tnoko/Clas_Olhson/')
+
 web.config.debug=False
 
 d=broadlink.discover(local_ip_address="192.168.1.11")
@@ -16,12 +17,23 @@ d=broadlink.discover(local_ip_address="192.168.1.11")
 for x in range(len(d)):
     print(d[x].name)
     if d[x].name == "Jaahdytin|7":
-        plugi=d[x]
+        plug1=d[x]
+    if d[x].name == "Tuuletin|7":
+        plug2=d[x]
+    if d[x].name == "pis|4":
+        plug3=d[x]
 
-plugi.auth()
+plug1.auth()
+plug2.auth()
+plug3.auth()
 
 class datoja:
     urls=()
+
+def otsikko(kuvio,x,y):
+    return('<div style="position: absolute; left:'+str(x)+'; top:'+str(y)+'">'+
+    '<h1>'+ kuvio + '</h1>'+
+    '</div>\n')
 
 def nappi(osoite,kuvio,x,y):
     return('<div style="position: absolute; left:'+str(x)+'; top:'+str(y)+'">'+
@@ -32,9 +44,15 @@ def palauta_paska(refresh=False):
     s='<html>'+\
        '<head><title>TOPSELIT</title>\n'+\
        '</head>\n'+\
-       '<h1> J&Auml;&Auml;HDYTIN </h1>'+\
-    nappi("ON","static/ON.png",100,100)+\
-    nappi("OFF","static/OFF.png",200,100)
+    otsikko(' J&Auml;&Auml;HDYTIN',100,30)+\
+    nappi("ON1","static/ON.png",100,100)+\
+    nappi("OFF1","static/OFF.png",200,100)+\
+    otsikko('TUULETIN',100,230)+\
+    nappi("ON2","static/ON.png",100,300)+\
+    nappi("OFF2","static/OFF.png",200,300)+\
+    otsikko('PI',100,430)+\
+    nappi("ON3","static/ON.png",100,500)+\
+    nappi("OFF3","static/OFF.png",200,500)
     return(s)
 
 datoja.urls=('/','index')
@@ -42,16 +60,40 @@ class index:
     def GET(self):
         return palauta_paska()
 
-datoja.urls+=('/ON','ON')
-class ON:
+datoja.urls+=('/ON1','ON1')
+class ON1:
     def GET(self):
-        plugi.set_power(True)
+        plug1.set_power(True)
         return palauta_paska()
 
-datoja.urls+=('/OFF','OFF')
-class OFF:
+datoja.urls+=('/OFF1','OFF1')
+class OFF1:
     def GET(self):
-        plugi.set_power(False)
+        plug1.set_power(False)
+        return palauta_paska()
+
+datoja.urls+=('/ON2','ON2')
+class ON2:
+    def GET(self):
+        plug2.set_power(True)
+        return palauta_paska()
+
+datoja.urls+=('/OFF2','OFF2')
+class OFF2:
+    def GET(self):
+        plug2.set_power(False)
+        return palauta_paska()
+
+datoja.urls+=('/ON3','ON3')
+class ON3:
+    def GET(self):
+        plug3.set_power(True)
+        return palauta_paska()
+
+datoja.urls+=('/OFF3','OFF3')
+class OFF3:
+    def GET(self):
+        plug3.set_power(False)
         return palauta_paska()
 
 os.environ["PORT"] = "8083"
